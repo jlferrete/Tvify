57a52de930199ff73335d9a1cd03398b9678a83c
Jose Luis Ferrete <joseluisferrete@gmail.com>
2018-04-29 12:44:21 +0200

    Fix shows without images in JSON

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $(function() {
 		shows.forEach(function (show) {
 			var article = template
 				.replace(':name:', show.name)
-				.replace(':img:', show.image.medium)
+				.replace(':img:', show.image ? show.image.medium : '')
 				.replace(':summary:', show.summary)
 				.replace(':img alt', show.name + " Logo")
 						
