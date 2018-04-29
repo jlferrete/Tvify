8e52484ff1a7194d63225cddf55d023950496a59
Jose Luis Ferrete <joseluisferrete@gmail.com>
2018-04-29 10:44:24 +0200

    Add AJAX request to TvMaze API and show in APP

--- index.js
+++ index.js
@@ -11,4 +11,30 @@ $(function() {
 			.val();
 		alert('Se ha buscado: ' + busqueda); 
 	})
+	
+	var template ='<article class="tv-show">' +
+          '<div class="left img-container">' +
+            '<img src=":img:" alt=":img alt:">' +
+          '</div>' +
+          '<div class="right info">' +
+            '<h1>:name:</h1>' +
+            '<p>:summary:</p> ' +
+          '</div>' +
+        '</article>';
+	
+	$.ajax({
+		url: 'http://api.tvmaze.com/shows',
+		success: function (shows, textStatus, xhr){
+			var $tvShowsContainer = $('#app-body').find('div.tv-shows');
+			shows.forEach(function (show) {
+				var article = template
+					.replace(':name:', show.name)
+					.replace(':img:', show.image.medium)
+					.replace(':summary:', show.summary)
+					.replace(':img alt', show.name + " Logo")
+							
+					$tvShowsContainer.append($(article))
+			})
+		}			
+	})
 })
