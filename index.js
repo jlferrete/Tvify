6696f3118944f9cb9cb3f499a78380116d72047e
Jose Luis Ferrete <joseluisferrete@gmail.com>
2018-04-29 09:57:29 +0200

    Add search form

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 $(function() {
-  
+  /**
+  * Submit search form
+  */
+  $('#app-body')
+	.find('form')
+	.submit(function (ev){
+		ev.preventDefault();
+		var busqueda = $(this)
+			.find('input[type="text"]')
+			.val();
+		alert('Se ha buscado: ' + busqueda); 
+	})
 })
